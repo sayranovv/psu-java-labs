48fa3c1c063bf00eb7e001edf473356391394265
sayranovv <sayranoveldar@gmail.com>
2026-01-17 12:17:20 +0500

    update main

--- lab-6/src/Main.java
+++ lab-6/src/Main.java
@@ -1,15 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import services.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        printWelcome();
+
+        while (running) {
+            printMenu();
+            String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "1":
+                    runTask(new Task1Service(), "Задание 1: @Invoke");
+                    break;
+                case "2":
+                    runTask(new Task2Service(), "Задание 2: @Default");
+                    break;
+                case "3":
+                    runTask(new Task3Service(), "Задание 3: @ToString");
+                    break;
+                case "4":
+                    runTask(new Task4Service(), "Задание 4: @Validate");
+                    break;
+                case "5":
+                    runTask(new Task5Service(), "Задание 5: @Two");
+                    break;
+                case "6":
+                    runTask(new Task6Service(), "Задание 6: @Cache");
+                    break;
+                case "0":
+                    running = false;
+                    System.out.println("\nПрограмма завершена.");
+                    break;
+                default:
+                    System.out.println("\nНеверный выбор, повторите ввод.\n");
+            }
+        }
+        scanner.close();
+    }
+
+    private static void printWelcome() {
+        System.out.println();
+        System.out.println("ЛАБОРАТОРНАЯ №6: АННОТАЦИИ И ТЕСТЫ");
+        System.out.println();
+    }
+
+    private static void printMenu() {
+        System.out.println("           МЕНЮ               ");
+        System.out.println("  1 - Задание 1: @Invoke");
+        System.out.println("  2 - Задание 2: @Default");
+        System.out.println("  3 - Задание 3: @ToString");
+        System.out.println("  4 - Задание 4: @Validate");
+        System.out.println("  5 - Задание 5: @Two");
+        System.out.println("  6 - Задание 6: @Cache");
+        System.out.println("  0 - Выход");
+        System.out.print("Ваш выбор: ");
+    }
+
+    private static void runTask(Object service, String taskName) {
+        try {
+            if (service instanceof Task1Service) {
+                ((Task1Service) service).runTask1();
+            } else if (service instanceof Task2Service) {
+                ((Task2Service) service).runTask2();
+            } else if (service instanceof Task3Service) {
+                ((Task3Service) service).runTask3();
+            } else if (service instanceof Task4Service) {
+                ((Task4Service) service).runTask4();
+            } else if (service instanceof Task5Service) {
+                ((Task5Service) service).runTask5();
+            } else if (service instanceof Task6Service) {
+                ((Task6Service) service).runTask6();
+            }
+
+            System.out.println("  Нажмите ENTER для возврата в меню  ");
+            new Scanner(System.in).nextLine();
+        } catch (Exception e) {
+            System.out.println("Ошибка при выполнении " + taskName + ": " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
