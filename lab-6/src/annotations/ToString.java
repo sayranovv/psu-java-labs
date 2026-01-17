68d61fde4aa54a590f925e87ff24d1e8b56d6a47
sayranovv <sayranoveldar@gmail.com>
2026-01-17 12:16:07 +0500

    add all annotations

---
+++ lab-6/src/annotations/ToString.java
@@ -0,0 +1,13 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ToString {
+    Mode value() default Mode.YES;
+    enum Mode { YES, NO }
+}
